a5a3b5a70fb7abd7b0b9df71273c188adefc695e
Tony Poon <tknpoon@gmail.com>
2017-12-01 14:26:48 +0800

    Update 01secmaster.sql

--- c02mysql/vol-initdb/01secmaster.sql
+++ c02mysql/vol-initdb/01secmaster.sql
@@ -2,27 +2,17 @@ CREATE USER 'sec_user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON secmaster.* TO 'sec_user'@'localhost';
 FLUSH PRIVILEGES;
 
-
-
 CREATE TABLE `exchange` (
   `id` int NOT NULL AUTO_INCREMENT,
   `abbrev` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO `exchange` (`abbrev`) VALUES (`HKG`);
-
-
-
 CREATE TABLE `data_vendor` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   PRIMARY KEY (`id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-INSERT INTO `data_vendor` (`name`) VALUES (`yahoo`);
-
-
 
 CREATE TABLE `symbol` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -34,25 +24,17 @@ CREATE TABLE `symbol` (
   KEY `index_exchange_id` (`exchange_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-CREATE VIEW `vSymbol` AS
-  SELECT e.abbrev exch, s.ticker ticker, s.name name, s.currency currency
-  FROM exchange e
-  INNER JOIN symbol s ON (s.exchange_id = e.id);
-
 CREATE TABLE `daily_price` (
   `id` int NOT NULL AUTO_INCREMENT, 
   `data_vendor_id` int NOT NULL,
   `symbol_id` int NOT NULL,
   `price_date` datetime NOT NULL,
-  `created_date` datetime NOT NULL,
-  `last_updated_date` datetime NOT NULL,
   `open_price` decimal(19,4) NULL,
   `high_price` decimal(19,4) NULL,
   `low_price` decimal(19,4) NULL,
   `close_price` decimal(19,4) NULL,
-  `adj_close_price` decimal(19,4) NULL,
   `volume` bigint NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`),  PRIMARY KEY (`id`),
   KEY `index_data_vendor_id` (`data_vendor_id`),
   KEY `index_synbol_id` (`symbol_id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
