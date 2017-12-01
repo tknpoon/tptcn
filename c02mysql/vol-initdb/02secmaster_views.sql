403892b309f7bade1a92ea1970a0e1e5a77f8f4f
Tony Poon <tknpoon@gmail.com>
2017-12-01 23:43:22 +0800

    Update 02secmaster_views.sql

--- c02mysql/vol-initdb/02secmaster_views.sql
+++ c02mysql/vol-initdb/02secmaster_views.sql
@@ -2,3 +2,6 @@ INSERT INTO `tVendor`(`name`) VALUES ("yahoo");
 INSERT INTO `tVendor`(`name`) VALUES ("hkex");
 INSERT INTO `tVendor`(`name`) VALUES ("hkma");
 
+INSERT INTO `tSymbol`(`symbol`, `preferredVendor`) VALUES ("0001.HK", "yahoo");
+INSERT INTO `tSymbol`(`symbol`, `preferredVendor`) VALUES ("0002.HK", "yahoo");
+
