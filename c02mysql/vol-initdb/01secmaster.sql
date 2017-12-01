5336eefd6776cf8dbfcb9fb5b981a4b868ba609a
Tony Poon <tknpoon@gmail.com>
2017-12-01 14:24:56 +0800

    Update 01secmaster.sql

--- c02mysql/vol-initdb/01secmaster.sql
+++ c02mysql/vol-initdb/01secmaster.sql
@@ -3,45 +3,42 @@ GRANT ALL PRIVILEGES ON secmaster.* TO 'sec_user'@'localhost';
 FLUSH PRIVILEGES;
 
 
+
 CREATE TABLE `exchange` (
   `id` int NOT NULL AUTO_INCREMENT,
   `abbrev` varchar(32) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `city` varchar(255) NULL,
-  `country` varchar(255) NULL,
-  `currency` varchar(64) NULL,
-  `timezone_offset` time NULL,
-  `created_date` datetime NOT NULL,
-  `last_updated_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO `exchange` (`abbrev`) VALUES (`HKG`);
+
 
 
 CREATE TABLE `data_vendor` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
-  `website_url` varchar(255) NULL,
-  `support_email` varchar(255) NULL, 
-  `created_date` datetime NOT NULL,
-  `last_updated_date` datetime NOT NULL,
   PRIMARY KEY (`id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO `data_vendor` (`name`) VALUES (`yahoo`);
+
 
 
 CREATE TABLE `symbol` (
   `id` int NOT NULL AUTO_INCREMENT,
   `exchange_id` int NULL,
   `ticker` varchar(32) NOT NULL,
-  `instrument` varchar(64) NOT NULL,
   `name` varchar(255) NULL, 
-  `sector` varchar(255) NULL,
   `currency` varchar(32) NULL,
-  `created_date` datetime NOT NULL,
-  `last_updated_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `index_exchange_id` (`exchange_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-  
+
+CREATE VIEW `vSymbol` AS
+  SELECT e.abbrev exch, s.ticker ticker, s.name name, s.currency currency
+  FROM exchange e
+  INNER JOIN symbol s ON (s.exchange_id = e.id);
+
 CREATE TABLE `daily_price` (
   `id` int NOT NULL AUTO_INCREMENT, 
   `data_vendor_id` int NOT NULL,
