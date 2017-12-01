1b02bbcca1f3c0529c51089552a514bce24d1863
Tony Poon <tknpoon@gmail.com>
2017-12-01 22:28:02 +0800

    Update 02secmaster_views.sql

--- c02mysql/vol-initdb/02secmaster_views.sql
+++ c02mysql/vol-initdb/02secmaster_views.sql
@@ -1,9 +1,3 @@
 INSERT INTO `exchange`(`abbrev`) VALUES ("HKG");
 
 INSERT INTO `data_vendor`(`name`) VALUES ("yahoo");
-
-CREATE VIEW `vSymbol` AS
- SELECT  e.abbrev exch, s.ticker ticker, s.name name, s.currency currency
- FROM    exchange e
- INNER JOIN symbol s ON (s.exchange_id = e.id);  
-
