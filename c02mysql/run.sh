f899002a28c921f1ab98f2788de2c3a844f87d43
Tony Poon <tknpoon@gmail.com>
2017-12-01 19:35:34 +0800

    Update run.sh

--- c02mysql/run.sh
+++ c02mysql/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-docker run --name c02mysql \
+  
+docker run \
+  -v $(dirname $0)/vol-datadir:/var/lib/mysql \
+  -v $(dirname $0)/vol-initdb:/docker-entrypoint-initdb.d \
+  --name c02mysql \
   -e MYSQL_ROOT_PASSWORD=useItOnce \
   -e MYSQL_ONETIME_PASSWORD=1 \
   -e MYSQL_DATABASE=secmaster \
-  -v $(dirname $0)/vol-datadir:/var/lib/mysql \
-  -v $(dirname $0)/vol-initdb:/docker-entrypoint-initdb.d \
-  -d mysql:5.7 \
+  -d \
+  mysql:5.7 \
   --character-set-server=utf8mb4 \
   --collation-server=utf8mb4_unicode_ci
