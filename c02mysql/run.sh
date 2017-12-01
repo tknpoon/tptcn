3b14a7c22a764c39e319bb354aff8d0a04c28e11
Tony Poon <tknpoon@gmail.com>
2017-12-01 21:20:36 +0800

    Update run.sh

--- c02mysql/run.sh
+++ c02mysql/run.sh
@@ -2,13 +2,16 @@
 
 CURDIR=`cd $(dirname $0); pwd`
 
+[ ! -d $CURDIR/vol-datadir ] && mkdir -p $CURDIR/vol-datadir
+[ ! -d $CURDIR/vol-initdb ]  && mkdir -p $CURDIR/vol-initdb
+
 docker run \
   -v $CURDIR/vol-datadir:/var/lib/mysql \
   -v $CURDIR/vol-initdb:/docker-entrypoint-initdb.d \
   --name c02mysql \
-  -e MYSQL_ROOT_PASSWORD=useItOnce \
-  -e MYSQL_ONETIME_PASSWORD=1 \
   -e MYSQL_DATABASE=secmaster \
+  -e MYSQL_ROOT_PASSWORD=useItOnce -e MYSQL_ONETIME_PASSWORD=yes\
+  -e MYSQL_USER=tptcn -e MYSQL_PASSWORD=tptcnpass\
   -p 12336:3306 \
   -d \
   mysql:5.7 \
