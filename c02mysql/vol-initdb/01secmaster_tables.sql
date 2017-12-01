dc8ed266f9b0ad2d7a5e5329326cd7756a07c047
Tony Poon <tknpoon@gmail.com>
2017-12-01 23:15:40 +0800

    Update 01secmaster_tables.sql

--- c02mysql/vol-initdb/01secmaster_tables.sql
+++ c02mysql/vol-initdb/01secmaster_tables.sql
@@ -1,38 +1,27 @@
-
-CREATE TABLE `exchange` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `abbrev` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `tVendor` (
+  `name` varchar(10) NOT NULL,
+  PRIMARY KEY (`name`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `data_vendor` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`) 
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `symbol` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `exchange_id` int NULL,
-  `ticker` varchar(32) NOT NULL,
+CREATE TABLE `tSymbol` (
+  `symbol` varchar(15) NOT NULL,
   `name` varchar(255) NULL, 
   `currency` varchar(32) NULL,
-  PRIMARY KEY (`id`),
-  KEY `index_exchange_id` (`exchange_id`)
+  PRIMARY KEY (`symbol`),
+  KEY `index_exchange` (`exchange`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `daily_price` (
-  `id` int NOT NULL AUTO_INCREMENT, 
-  `data_vendor_id` int NOT NULL,
-  `symbol_id` int NOT NULL,
+CREATE TABLE `tDailyPrice` (
+  `vendor_name` varchar(10) NOT NULL,
+  `symbol` varchar(15) NOT NULL,
   `price_date` datetime NOT NULL,
   `open_price` decimal(19,4) NULL,
   `high_price` decimal(19,4) NULL,
   `low_price` decimal(19,4) NULL,
   `close_price` decimal(19,4) NULL,
   `volume` bigint NULL,
-  PRIMARY KEY (`id`),
-  KEY `index_data_vendor_id` (`data_vendor_id`),
-  KEY `index_synbol_id` (`symbol_id`) 
+  PRIMARY KEY (`symbol`,`price_date`,`vendor_name`),
+  KEY `index_vendor` (`vendor_name`),
+  KEY `index_symbol` (`symbol`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
