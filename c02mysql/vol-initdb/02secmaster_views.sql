13b217b39aff1e3916de419d76ab143c9fa3da05
Tony Poon <tknpoon@gmail.com>
2017-12-01 23:22:00 +0800

    Update 02secmaster_views.sql

--- c02mysql/vol-initdb/02secmaster_views.sql
+++ c02mysql/vol-initdb/02secmaster_views.sql
@@ -1,3 +1,10 @@
-INSERT INTO `exchange`(`abbrev`) VALUES ("HKG");
+INSERT INTO `tVendor`(`name`) VALUES ("yahoo");
+INSERT INTO `tVendor`(`name`) VALUES ("hkex");
+INSERT INTO `tVendor`(`name`) VALUES ("hkma");
 
-INSERT INTO `data_vendor`(`name`) VALUES ("yahoo");
+CREATE VIEW `vDaily` AS
+  SELECT  d.symbol symbol,
+    d.price_date date, 
+    d.open_price open, d.high_price high, d.low_price low, d.close_price close, d.volume volume
+  FROM    tDailyPrice d;  
+
