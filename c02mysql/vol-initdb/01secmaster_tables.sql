424c00b524aa534f9d60fd8b6c190730dca8161a
Tony Poon <tknpoon@gmail.com>
2017-12-01 23:38:36 +0800

    Update 01secmaster_tables.sql

--- c02mysql/vol-initdb/01secmaster_tables.sql
+++ c02mysql/vol-initdb/01secmaster_tables.sql
@@ -7,20 +7,21 @@ CREATE TABLE `tSymbol` (
   `symbol` varchar(15) NOT NULL,
   `name` varchar(255) NULL, 
   `currency` varchar(32) NULL,
+  `preferredVendor` varchar(10) NULL,
   PRIMARY KEY (`symbol`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tDailyPrice` (
-  `vendor_name` varchar(10) NOT NULL,
+  `vendorName` varchar(10) NOT NULL,
   `symbol` varchar(15) NOT NULL,
-  `price_date` datetime NOT NULL,
-  `open_price` decimal(19,4) NULL,
-  `high_price` decimal(19,4) NULL,
-  `low_price` decimal(19,4) NULL,
-  `close_price` decimal(19,4) NULL,
+  `priceDate` datetime NOT NULL,
+  `openPrice` decimal(19,4) NULL,
+  `highPrice` decimal(19,4) NULL,
+  `lowPrice` decimal(19,4) NULL,
+  `closePrice` decimal(19,4) NULL,
   `volume` bigint NULL,
-  PRIMARY KEY (`symbol`,`price_date`,`vendor_name`),
-  KEY `index_vendor` (`vendor_name`),
+  PRIMARY KEY (`symbol`,`priceDate`,`vendorName`),
+  KEY `index_vendor` (`vendorName`),
   KEY `index_symbol` (`symbol`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
