d52ab1b97d03242318c48c730e36d96042a00afc
Tony Poon <tknpoon@gmail.com>
2017-12-02 00:00:48 +0800

    Update 02secmaster_views.sql

--- c02mysql/vol-initdb/02secmaster_views.sql
+++ c02mysql/vol-initdb/02secmaster_views.sql
@@ -6,7 +6,11 @@ INSERT INTO `tSymbol`(`symbol`, `preferredVendor`) VALUES ("0001.HK", "yahoo");
 INSERT INTO `tSymbol`(`symbol`, `preferredVendor`) VALUES ("0002.HK", "yahoo");
 
 INSERT INTO `tDailyPrice` (`vendorName`, `symbol`, `priceDate`, `openPrice`, `highPrice`, `lowPrice`, `closePrice`, `volume`)
+  VALUES ("yahoo", "0002.HK", "2017-11-01", 1,2,0,2,20);
+INSERT INTO `tDailyPrice` (`vendorName`, `symbol`, `priceDate`, `openPrice`, `highPrice`, `lowPrice`, `closePrice`, `volume`)
   VALUES ("yahoo", "0002.HK", "2017-12-01", 1,2,0,2,20);
+INSERT INTO `tDailyPrice` (`vendorName`, `symbol`, `priceDate`, `openPrice`, `highPrice`, `lowPrice`, `closePrice`, `volume`)
+  VALUES ("yahoo", "0001.HK", "2017-12-01", 2,2,0,2,20);
 
 CREATE VIEW `vDaily` AS
   SELECT d.`symbol` AS `symbol`,
