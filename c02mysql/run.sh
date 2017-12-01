f6ccef77c4ae5256054962a7f16da555804fdc8e
Tony Poon <tknpoon@gmail.com>
2017-12-01 13:09:57 +0800

    Update run.sh

--- c02mysql/run.sh
+++ c02mysql/run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 docker run --name c02mysql \
-  -e MYSQL_ROOT_PASSWORD=my-secret-pw \
-  -d mysql:5.7 \  -d mysql:5.7 \
+  -e MYSQL_ROOT_PASSWORD=useItOnce \
+  -e MYSQL_ONETIME_PASSWORD=1 \
+  -d mysql:5.7 \
   --character-set-server=utf8mb4 \
   --collation-server=utf8mb4_unicode_ci
-
-$ docker run --name some-mysql -v /my/custom:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:tag
-
