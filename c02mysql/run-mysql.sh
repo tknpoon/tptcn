97236c14dc9598b8105db7e69c3da810441486f5
tknpoon <tknpoon@gmail.com>
2017-12-02 16:20:03 +0800

    test

--- c02mysql/run-mysql.sh
+++ c02mysql/run-mysql.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-CURDIR=`cd $(dirname $0); pwd`
+CONTAINER_NAME=c02mysql
 
-[ ! -d $CURDIR/vol-datadir ] && mkdir -p $CURDIR/vol-datadir
+CURDIR=`cd $(dirname $0); pwd`
+VOLDIR=$HOME/vol/$CONTAINER_NAME
+
+[ ! -d $VOLDIR/vol-datadir ] && mkdir -p $VOLDIR/vol-datadir
 [ ! -d $CURDIR/vol-initdb ]  && mkdir -p $CURDIR/vol-initdb
 
 docker run \
- -v $CURDIR/vol-datadir:/var/lib/mysql \
+ -v $VOLDIR/vol-datadir:/var/lib/mysql \
  -v $CURDIR/vol-initdb:/docker-entrypoint-initdb.d \
- --name c02mysql \
+ --name $CONTAINER_NAME \
  -e MYSQL_DATABASE=secmaster \
  -e MYSQL_ROOT_PASSWORD=useItOnce -e MYSQL_ONETIME_PASSWORD=yes \
  -e MYSQL_USER=tptcn -e MYSQL_PASSWORD=tptcnpass \
