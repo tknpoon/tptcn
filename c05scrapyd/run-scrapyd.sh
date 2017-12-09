cf8e680b0b7e4e2b2ec0b806858d3bfcc9aa79e5
Tony Poon <tknpoon@gmail.com>
2017-12-09 14:17:39 +0800

    c05scrapyd

--- c05scrapyd/run-scrapyd.sh
+++ c05scrapyd/run-scrapyd.sh
@@ -6,11 +6,10 @@ CONTAINER_NAME=c05scrapyd
 CURDIR=`cd $(dirname $0); pwd`
 VOLDIR=$HOME/vol/$CONTAINER_NAME
 
-[ ! -d $VOLDIR/vol-etc_scrapyd ] && mkdir -p $VOLDIR/vol-etc_scrapyd
 [ ! -d $VOLDIR/vol-lib_scrapyd ] && mkdir -p $VOLDIR/vol-lib_scrapyd
 
 docker run \
- -v $VOLDIR/vol-etc_scrapyd:/etc/scrapyd \
+ -v $CURDIR/scrapyd.conf:/etc/scrapyd/scrapyd.conf \
  -v $VOLDIR/vol-lib_scrapyd:/var/lib/scrapyd \
  --link c04nginx:web \
  --link c02mysql:db \
