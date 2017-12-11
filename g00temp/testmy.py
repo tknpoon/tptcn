1235fe79a9c959075a23419820bb8b2eda1f9794
Tony Poon <tknpoon@gmail.com>
2017-12-11 21:28:36 +0800

    20171211

--- g00temp/testmy.py
+++ g00temp/testmy.py
@@ -6,33 +6,53 @@ import datetime as dt
 import MySQLdb as my
 
 import pandas_datareader.data as web
+from multiprocessing import Pool
 
-
-conn = my.connect(host='db', user=os.environ['MYSQL_USER'],passwd=os.environ['MYSQL_PASSWORD'],db=os.environ['MYSQL_DB'])
-
-conn.query("""
-select symbol from tSymbol where availVendors like "%test%"
-""")
-r=conn.use_result()
-
-######
-while (True) :
-    row = r.fetch_row()
-    if len(row) == 0: break
-
-    symbol = row[0][0]
-
+##############################
+def grabyahoo(symbol):
     source='yahoo'
     start='01/01/1990'
-    end='01/01/2018'
-    
+    end=dt.date.today().strftime('%m/%d/%Y')
     qt= web.DataReader(symbol, source, start, end)
     print qt.tail()
+    return symbol
 
-    time.sleep(1)
-
-conn.close()
-
-
-
-
+    
+##############################
+# main
+if __name__ == '__main__':
+    conn = my.connect(host='db', user=os.environ['MYSQL_USER'],passwd=os.environ['MYSQL_PASSWORD'],db=os.environ['MYSQL_DB'])
+    conn.query("select symbol from tSymbol where availVendors like '%test%' ")
+    r=conn.use_result()
+    
+    symlist=[]
+    while (True) :
+        row = r.fetch_row()
+        if len(row) == 0: break
+        symlist.append(row[0][0])
+    conn.close()
+        
+    p = Pool(3)
+    print(p.map(grabyahoo, symlist))
+    
+#while (True) :
+#    row = r.fetch_row()
+#    if len(row) == 0: break
+#
+#    symbol = row[0][0]
+#
+#    source='yahoo'
+#    start='01/01/1990'
+#    end='01/01/2018'
+#    
+#    qt= web.DataReader(symbol, source, start, end)
+#    print qt.tail()
+#
+#    time.sleep(1)
+#
+#conn.close()
+#
+#
+#
+#
+#
