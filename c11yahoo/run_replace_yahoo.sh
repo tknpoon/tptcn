ed72eba57fd277ec1599ae3b69fe56b9ef1cd1ff
Tony Poon <tknpoon@gmail.com>
2017-12-12 21:14:30 +0800

    Create run_replace_yahoo.sh

---
+++ c11yahoo/run_replace_yahoo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+. $HOME/.self_env
+
+CURDIR=`cd $(dirname $0);pwd`
+CONTAINER_NAME=$(basename `(cd $CURDIR; pwd)`)
+
+VOLDIR=$HOME/vol/$CONTAINER_NAME
+SCRIPTDIR=$CURDIR/scripts
+
+[ ! -d $VOLDIR/vol-working ] && mkdir -p $VOLDIR/vol-working
+
+docker run \
+ -v $VOLDIR/vol-working:/working \
+ -v $SCRIPTDIR:/scripts \
+ --link c02mysql:db \
+ -e MYSQL_USER=$MYSQL_USER \
+ -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+ -e MYSQL_DB=$MYSQL_DB \
+ --rm -ti \
+ tknpoon/private:$CONTAINER_NAME \
+ python /scripts/grab_yahoo.py
