bd105c7a7bbd4d42af5ba93b9d187fa2688ee93c
Tony Poon <tknpoon@gmail.com>
2017-12-12 21:21:04 +0800

    Update run-replace_yahoo.sh

--- c11yahoo/run-replace_yahoo.sh
+++ c11yahoo/run-replace_yahoo.sh
@@ -18,4 +18,4 @@ docker run \
  -e MYSQL_DB=$MYSQL_DB \
  --rm -ti \
  tknpoon/private:$CONTAINER_NAME \
- mysql --host=db --database=$MYSQL_DB --user=$MYSQL_USER --password=$MYSQL_PASSWORD < /scripts/replace_yahoo.sql
+ bash -c "mysql --host=db --database=$MYSQL_DB --user=$MYSQL_USER --password=$MYSQL_PASSWORD < /scripts/replace_yahoo.sql"
