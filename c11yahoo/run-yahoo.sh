0cc8752c8cea156fb333f3a4d73555c3561e9f0d
Tony Poon <tknpoon@gmail.com>
2017-12-12 18:41:50 +0800

    20171212

--- c11yahoo/run-yahoo.sh
+++ c11yahoo/run-yahoo.sh
@@ -12,6 +12,10 @@ SCRIPTDIR=$CURDIR/scripts
 docker run \
  -v $VOLDIR/vol-working:/working \
  -v $SCRIPTDIR:/scripts \
+ --link c02mysql:db \
+ -e MYSQL_USER=$MYSQL_USER \
+ -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+ -e MYSQL_DB=$MYSQL_DB \
  --name $CONTAINER_NAME \
  --rm -ti \
  tknpoon/private:$CONTAINER_NAME \
