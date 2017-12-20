ebf8c4638844c575701b0afdb48fd5eda09fb2ca
tknpoon <tknpoon@gmail.com>
2017-12-20 23:01:30 +0800

    20171220-2301

--- c11yahoo/scripts/grab_yahoo.py
+++ c11yahoo/scripts/grab_yahoo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import json,math,time,os
+import json,math,time,os, sys
 import datetime as dt
 #import _mysql as my
 import MySQLdb as my
@@ -8,6 +8,9 @@ import MySQLdb as my
 import pandas_datareader.data as web
 from multiprocessing import Pool
 from sqlalchemy import create_engine
+
+## global variables
+grabAll = False
 
 ##############################
 def save_sql(symbol, df):
@@ -32,13 +35,14 @@ def grabyahoo(symbol):
     end=dt.date.today().strftime('%m/%d/%Y')
     ##
     conn = my.connect(host='db', user=os.environ['MYSQL_USER'],passwd=os.environ['MYSQL_PASSWORD'],db=os.environ['MYSQL_DB'])
-    conn.query("select max(Date) from tDailyPrice_yahoo where symbol = '%s' " % (symbol) )
-    r=conn.use_result()
-    while (True) :
-        row = r.fetch_row()
-        if len(row) == 0 or row[0][0] is None: break
-        start= row[0][0].strftime('%m/%d/%Y')
-    conn.close()
+    if not grabAll:
+        conn.query("select max(Date) from tDailyPrice_yahoo where symbol = '%s' " % (symbol) )
+        r=conn.use_result()
+        while (True) :
+            row = r.fetch_row()
+            if len(row) == 0 or row[0][0] is None: break
+            start= row[0][0].strftime('%m/%d/%Y')
+        conn.close()
 
     try:
         qt= web.DataReader(symbol, source, start, end)
@@ -55,13 +59,17 @@ def grabyahoo(symbol):
 ##############################
 # main
 if __name__ == '__main__':
+
+    if len(sys.argv) > 1 and sys.argv[1] == "all":
+        grabAll = True
+
     #Get a list of RIC
     symlist=[]
     conn = my.connect(host='db', user=os.environ['MYSQL_USER'],passwd=os.environ['MYSQL_PASSWORD'],db=os.environ['MYSQL_DB'])
     conn.query("select symbol from tSymbol where availVendors like '%yahoo%' ")
     r=conn.use_result()
     
-    while (True) :
+    while (True):
         row = r.fetch_row()
         if len(row) == 0: break
         symlist.append(row[0][0])
