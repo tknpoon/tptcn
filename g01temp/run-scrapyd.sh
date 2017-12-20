8423a5d2d4a1d254d85f5d68eb33d5673c4dfb65
tknpoon <tknpoon@gmail.com>
2017-12-20 23:10:28 +0800

    20171220-2310

---
+++ g01temp/run-scrapyd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. $HOME/.self_env
+
+CONTAINER_NAME=g01temp
+
+CURDIR=`cd $(dirname $0); pwd`
+VOLDIR=$HOME/vol/$CONTAINER_NAME
+
+[ ! -d $VOLDIR/vol-datadir ] && mkdir -p $VOLDIR/vol-datadir
+
+docker run \
+ -v $VOLDIR/vol-datadir:/scrapyproj \
+ --name $CONTAINER_NAME \
+ -p 12080:6800 \
+ -d \
+ vimagick/scrapyd
