9fd523ec5ecf3168cfea4671920c6750891425b0
tknpoon <tknpoon@gmail.com>
2018-01-01 14:59:31 +0800

    20180101-1459

---
+++ c03vpnd/run-vpnd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+. $HOME/.self_env
+
+sudo modprobe af_key
+
+CONTAINER_NAME=c03vpnd
+
+CURDIR=`cd $(dirname $0); pwd`
+VOLDIR=$HOME/vol/$CONTAINER_NAME
+CHAPSECRET=${VOLDIR}/vol-datadir/chap-secret
+
+docker run \
+ --name c03vpnd \
+ --env-file ~/.self_env \
+ --restart=always \
+ -p 500:500/udp \
+ -p 4500:4500/udp \
+ -v /lib/modules:/lib/modules:ro \
+ -d --privileged \
+ hwdsl2/ipsec-vpn-server
