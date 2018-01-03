d139186efca6f935d867aadd7543db5fe27c228f
tknpoon <tknpoon@gmail.com>
2018-01-03 22:51:40 +0800

    20180103-2251

--- c00self/grab2115.sh
+++ c00self/grab2115.sh
@@ -27,34 +27,42 @@ fi
 
 ##hsi con
 url=`date -d $dstr +https://www.hsi.com.hk/HSI-Net/static/revamp/contents/en/indexes/report/hsi/con_%-d%b%y.csv`
+[ ! -d ~/store/raw/hsi ] && mkdir -p ~/store/raw/hsi
 dlzip $url ~/store/raw/hsi/$(basename $url)
 
 ##hsi idx perf
 url=`date -d $dstr +https://www.hsi.com.hk/HSI-Net/static/revamp/contents/en/indexes/Index_Performance_Summary_%-d%b%y.xls`
+[ ! -d ~/store/raw/hsi ] && mkdir -p ~/store/raw/hsi
 dlzip $url ~/store/raw/hsi/$(basename $url)
 
 #hkma
 url=`date -d $dstr +http://www.hkma.gov.hk/eng/market-data-and-statistics/monetary-statistics/monetary-base/%Y/%Y%m%d-2.shtml`
+[ ! -d ~/store/raw/hkma/$(date -d $dstr +%Y) ] && mkdir -p ~/store/raw/hkma/$(date -d $dstr +%Y)
 dlzip $url ~/store/raw/hkma/$(date -d $dstr +%Y)/moneybase$(basename $url .shtml).html
 
 #hkex_quot
 url=`date -d $dstr +http://www.hkex.com.hk/eng/stat/smstat/dayquot/d%y%m%de.htm`
+[ ! -d ~/store/raw/hkex_quot/$(date -d $dstr +%Y) ] && mkdir -p ~/store/raw/hkex_quot/$(date -d $dstr +%Y)
 dlzip $url ~/store/raw/hkex_quot/$(date -d $dstr +%Y)/$(basename $url)
 
 #hkex_gem
 url=`date -d $dstr +http://www.hkgem.com/statistics/daily/e_G%y%m%d.htm`
+[ ! -d ~/store/raw/hkex_gem/$(date -d $dstr +%Y) ] && mkdir -p ~/store/raw/hkex_gem/$(date -d $dstr +%Y)
 dlzip $url ~/store/raw/hkex_gem/$(date -d $dstr +%Y)/$(basename $url)
 
 #hhio
 url=`date -d $dstr +http://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hhio%y%m%d.zip`
+[ ! -d ~/store/raw/hkex_hhio/$(date -d $dstr +%Y) ] && mkdir -p ~/store/raw/hkex_hhio/$(date -d $dstr +%Y)
 dl $url ~/store/raw/hkex_hhio/$(date -d $dstr +%Y)/$(basename $url)
 
 #hhio
 url=`date -d $dstr +http://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hsio%y%m%d.zip`
+[ ! -d ~/store/raw/hkex_hsio/$(date -d $dstr +%Y) ] && mkdir -p ~/store/raw/hkex_hsio/$(date -d $dstr +%Y)
 dl $url ~/store/raw/hkex_hsio/$(date -d $dstr +%Y)/$(basename $url)
 
 #dqe
 url=`date -d $dstr +http://www.hkex.com.hk/eng/stat/dmstat/dayrpt/dqe%y%m%d.zip`
+[ ! -d ~/store/raw/hkex_stko/$(date -d $dstr +%Y) ] && mkdir -p ~/store/raw/hkex_stko/$(date -d $dstr +%Y)
 dl $url ~/store/raw/hkex_stko/$(date -d $dstr +%Y)/$(basename $url)
 
 
