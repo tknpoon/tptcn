e74b3ac259331a542e2795127dfb710088866ece
tknpoon <tknpoon@gmail.com>
2018-01-27 18:38:53 +0800

    20180127-1838

--- c01telegram/run-telegram.sh
+++ c01telegram/run-telegram.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-. $HOME/.self_env
+#. $HOME/.self_env
 
 CONTAINER_NAME=c01telegram
 
 docker run \
  --name $CONTAINER_NAME \
- -e TOKEN=$TELEGRAM_TOKEN \
- -e CHAT_ID=$TELEGRAM_CHAT_ID \
+ --env-file $HOME/.self_env \
  -p 11025:25 \
  -d \
  --restart=always \
  tknpoon/private:$CONTAINER_NAME \
  python /telegram.py
+
+# -e TOKEN=$TELEGRAM_TOKEN \
+# -e CHAT_ID=$TELEGRAM_CHAT_ID \
