6cd8fa1a681b4e209ecb67bf35b0750ec0d713a6
tknpoon <tknpoon@gmail.com>
2018-02-03 10:24:23 +0800

    20180203-1024

--- c02mysql/backup-secmaster-03-hkex.sh
+++ c02mysql/backup-secmaster-03-hkex.sh
@@ -7,12 +7,11 @@ VOLDIR=$HOME/vol/$CONTAINER_NAME
 
 docker exec \
  $CONTAINER_NAME \
- bash -c 'exec mysqldump \
+ bash -c 'echo USE secmaster\; ; echo; exec mysqldump \
  -u$MYSQL_USER -p"$MYSQL_PASSWORD" \
  --no-create-info \
  --databases $MYSQL_DATABASE \
  --tables \
-  tDailyPrice \
   tTR_Daily \
   tYAHOO_Daily \
   tHKEX_Quotation \
