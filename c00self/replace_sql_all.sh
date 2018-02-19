0e6d741721ee286a190ec2e152a022bd8ab6784e
tknpoon <tknpoon@gmail.com>
2018-02-19 12:13:48 +0800

    20180219-1213

---
+++ c00self/replace_sql_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CONTAINER_NAME=c02mysql
+
+CURDIR=`cd $(dirname $0); pwd`
+
+for s in $CURDIR/sql/*sql;do
+  echo =============== working on $s `date`
+  cat $s | \
+    docker exec -i \
+      $CONTAINER_NAME \
+      bash -c 'mysql $MYSQL_DATABASE -u$MYSQL_USER -p"$MYSQL_PASSWORD"'
+done
+
+fd=1990-01-01
+td=$(date +'%Y-%m-%d')
+echo "call pHKEXquote('$fd', '$td'); call pHKEXupdateOpen('$fd', '$td');" | \
+  docker exec -i \
+    $CONTAINER_NAME \
+    bash -c 'exec mysql $MYSQL_DATABASE -u$MYSQL_USER -p"$MYSQL_PASSWORD"'
+
