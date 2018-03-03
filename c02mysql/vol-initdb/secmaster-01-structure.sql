71d17120f745ccb5b3689b3e40e2e89cbcffe141
tknpoon <tknpoon@gmail.com>
2018-03-03 17:46:20 +0800

    20180303-1746

--- c02mysql/vol-initdb/secmaster-01-structure.sql
+++ c02mysql/vol-initdb/secmaster-01-structure.sql
@@ -210,7 +210,6 @@ CREATE TABLE `tYAHOO_Daily` (
 -- Dumping routines for database 'secmaster'
 --
 /*!50003 DROP PROCEDURE IF EXISTS `pDaily` */;
-ALTER DATABASE `secmaster` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -221,14 +220,13 @@ ALTER DATABASE `secmaster` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`tpuser`@`%` PROCEDURE `pDaily`(IN `symbol` VARCHAR(15))
-SELECT * FROM vDaily
-  WHERE vDaily.symbol = symbol ;;
+SELECT * FROM tDailyPrice
+  WHERE tDailyPrice.symbol = symbol ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-ALTER DATABASE `secmaster` CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
 /*!50003 DROP PROCEDURE IF EXISTS `pHKEXquote` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -340,4 +338,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-24 22:32:08
+-- Dump completed on 2018-03-03  9:45:56
