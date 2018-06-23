44e4f5b9aaf45f3dd717ecd7ad72f411814935d7
tknpoon <tknpoon@gmail.com>
2018-06-24 05:38:03 +0800

    20180624-0538

--- c07noip/run-noip.sh
+++ c07noip/run-noip.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+THEVOL=c07noip
+[ $(hostname) == tphome201 ] && THEVOL=c07noip-tphome201
+
 docker run \
  --name c07noip \
  --env-file $HOME/.self_env \
  -d --rm \
  -v /etc/localtime:/etc/localtime \
- -v $HOME/vol/c07noip:/config \
+ -v $HOME/vol/$THEVOL:/config \
  coppit/no-ip
 
 
