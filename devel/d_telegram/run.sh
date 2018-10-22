206d3c45df052e55f4260ba4843f083e089b29c8
Tony Poon <tknpoon@gmail.com>
2018-10-22 22:33:18 +0800

    Update run.sh

--- devel/d_telegram/run.sh
+++ devel/d_telegram/run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-
 DIRNAME=`dirname $0`
 TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
+
+[ "${TAG_NAME:0:2}" == "d_" ] && PORTBASE=20000
+[ "${TAG_NAME:0:2}" == "u_" ] && PORTBASE=30000
+[ "${TAG_NAME:0:2}" == "p_" ] && PORTBASE=40000
+
+PORT25=`expr $PORTBASE + 25`
 
 docker run \
  --name $TAG_NAME \
  --env-file $HOME/.self_env \
- -p 20025:25 \
+ -p ${PORT25}:25 \
  -d \
- --network d_tptcn_overlay \
+ --network ${TAG_NAME:0:2}tptcn_overlay \
  --restart=always \
  tknpoon/private:$TAG_NAME \
  python /telegram.py
