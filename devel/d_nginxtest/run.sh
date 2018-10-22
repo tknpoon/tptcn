89e38ff68c79ad531315227365f1221084a61edc
Tony Poon <tknpoon@gmail.com>
2018-10-22 22:34:30 +0800

    Update run.sh

--- devel/d_nginxtest/run.sh
+++ devel/d_nginxtest/run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-
 DIRNAME=`dirname $0`
 TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
+
+[ "${TAG_NAME:0:2}" == "d_" ] && PORTBASE=20000
+[ "${TAG_NAME:0:2}" == "u_" ] && PORTBASE=30000
+[ "${TAG_NAME:0:2}" == "p_" ] && PORTBASE=40000
+
+PORT80=`expr $PORTBASE + 80`
 
 docker run \
  --name $TAG_NAME \
  --env-file $HOME/.self_env \
  -d --rm \
- --network d_tptcn_overlay \
- -p 20080:80 \
+ --network ${TAG_NAME:0:2}tptcn_overlay \
+ -p ${PORT80}:80 \
  nginx:alpine
-
