3411a45c6033aa35c46aeb84858dde99fa456372
tknpoon <tknpoon@gmail.com>
2018-10-22 21:14:26 +0800

    20181022-2114

---
+++ dev/d_test1/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+DIRNAME=`dirname $0`
+TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
+
+docker run \
+ --name $TAG_NAME \
+ --env-file $HOME/.self_env \
+ -d --rm \
+ alpine \
+ ash -c 'while [ 1 ]; do sleep 5; done'
+
