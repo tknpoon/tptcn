290422b36716927626bd0a15cb2ba1bd2eab65eb
ubuntu <ubuntu@tphome201.local>
2018-10-27 23:35:08 +0800

    xmysql

---
+++ devel/d_xmysql/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIRNAME=`dirname $0`
+TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
+
+[ "${TAG_NAME:0:2}" == "d_" ] && PORTBASE=20000
+[ "${TAG_NAME:0:2}" == "u_" ] && PORTBASE=30000
+[ "${TAG_NAME:0:2}" == "p_" ] && PORTBASE=40000
+[ "${TAG_NAME:0:2}" == "g_" ] && PORTBASE=50000
+
+PORT3000=`expr $PORTBASE + 3000`
+
+CURDIR=`cd $(dirname $0); pwd`
+VOLDIR=$HOME/vol/$TAG_NAME
+
+docker run \
+ --name $TAG_NAME \
+ --env-file $HOME/.self_env \
+ -e DB_HOST=${TAG_NAME:0:2}mysql \
+ -e DB_DATABASE=${TAG_NAME:0:2}master \
+ --network ${TAG_NAME:0:2}tptcn_overlay \
+ -d \
+ -p $(ifconfig -a | grep inet|grep 192.168.8.|cut -d: -f2|cut -d' ' -f1):${PORT3000}:3000 \
+tknpoon/private:$TAG_NAME \
+bash -c 'xmysql -h $DB_HOST -u $DB_USER -p $DB_PASS -d $DB_DATABASE -r 0.0.0.0'
