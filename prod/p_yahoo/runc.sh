5ddf8f1f8672aadcf0f660a5ed7d09e4adaa2301
ubuntu <ubuntu@tphome201.local>
2018-11-05 23:10:55 +0800

    daemon and console

---
+++ prod/p_yahoo/runc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIRNAME=`dirname $0`
+CURDIR=`cd $(dirname $0); pwd`
+TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
+
+[ "${TAG_NAME:0:2}" == "d_" ] && PORTBASE=20000
+[ "${TAG_NAME:0:2}" == "u_" ] && PORTBASE=30000
+[ "${TAG_NAME:0:2}" == "p_" ] && PORTBASE=40000
+[ "${TAG_NAME:0:2}" == "g_" ] && PORTBASE=50000
+
+docker run \
+ --name ${TAG_NAME}_`date +%s` \
+ --env-file $HOME/.self_env \
+ -e STAGE=${TAG_NAME:0:1} \
+ -v ${CURDIR}/entrypoint.py:/entrypoint.py \
+ --network ${TAG_NAME:0:2}tptcn_overlay \
+ --rm \
+ tknpoon/private:$TAG_NAME \
+ python /entrypoint.py 
+
+# -ti \
+
---
+++ uat/u_scrapy_hkma/runc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DIRNAME=`cd $(dirname $0); pwd`
+CURDIR=`cd $(dirname $0); pwd`
+TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
+
+[ "${TAG_NAME:0:2}" == "d_" ] && PORTBASE=20000
+[ "${TAG_NAME:0:2}" == "u_" ] && PORTBASE=30000
+[ "${TAG_NAME:0:2}" == "p_" ] && PORTBASE=40000
+[ "${TAG_NAME:0:2}" == "g_" ] && PORTBASE=50000
+
+
+today=base$(date +%Y%m%d)
+#today=base201811
+#file:///127.0.0.1/path/to/file.html
+for hkmafile in `(cd $HOME/store/; find raw/hkma -name \*${today}\*html\*)`
+do
+ #url=`printf "http://${TAG_NAME:0:1}_nginx/%s" $hkmafile | sed -e 's/.gz$//'`
+ #url=`printf "file:///127.0.0.1/tmp/store/%s" $hkmafile | sed -e 's/.gz$//'`
+ gzfile=`printf "/tmp/store/%s" $hkmafile`
+
+ docker run \
+ --name $TAG_NAME \
+ --network ${TAG_NAME:0:1}_tptcn_overlay \
+ --env-file $HOME/.self_env \
+ -e URL_TO_SCRAP=file:///`basename $gzfile .gz` \
+ -e STAGE=${TAG_NAME:0:1} \
+ -e GZFILE_TO_SCRAP=$gzfile \
+ -v $CURDIR/entrypoint.py:/var/lib/scrapyd/entrypoint.py \
+ -v $HOME/store:/tmp/store \
+ --rm \
+ vimagick/scrapyd \
+ /bin/bash -c 'cp $GZFILE_TO_SCRAP / ; cd /;gunzip `basename $GZFILE_TO_SCRAP`; scrapy runspider /var/lib/scrapyd/entrypoint.py'
+done
