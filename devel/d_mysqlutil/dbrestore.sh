aa550ea7ff623245f1250eb1053bd010665cd9f8
tknpoon <tknpoon@gmail.com>
2018-11-14 22:34:09 +0800

    20181114-2234

--- devel/d_mysqlutil/dbrestore.sh
+++ devel/d_mysqlutil/dbrestore.sh
@@ -6,11 +6,7 @@ TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
 STAGE=${TAG_NAME:0:1}
 
 THE_DB=${STAGE}_master
-<<<<<<< HEAD
 CMD="mysql -uroot -p\"\$MYSQL_ROOT_PASSWORD\" $THE_DB $@"
-=======
-CMD="mysql -uroot -p\$MYSQL_ROOT_PASSWORD $THE_DB $@"
->>>>>>> 540a1aef18164eedf033e15ce9b6916666b4f488
 
 docker exec -i \
   g_mysql \
