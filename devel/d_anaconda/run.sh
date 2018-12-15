582282a153341a963c2f3a52f62e0f8251e8af84
tknpoon <tknpoon@gmail.com>
2018-12-15 22:19:40 +0800

    20181215-2219

---
+++ devel/d_anaconda/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIRNAME=`dirname $0`
+TAG_NAME=$(cd $DIRNAME ; basename `pwd`)
+
+[ "${TAG_NAME:0:2}" == "d_" ] && PORTBASE=20000
+[ "${TAG_NAME:0:2}" == "u_" ] && PORTBASE=30000
+[ "${TAG_NAME:0:2}" == "p_" ] && PORTBASE=40000
+[ "${TAG_NAME:0:2}" == "g_" ] && PORTBASE=50000
+
+PORT=`expr $PORTBASE + 8888`
+
+docker run \
+ --name $TAG_NAME \
+ --env-file $HOME/.self_env \
+ --expose ${PORT} --publish-all \
+ -d --rm \
+ --network ${TAG_NAME:0:2}tptcn_overlay \
+ tknpoon/private:$TAG_NAME \
+ jupyter notebook --allow-root --notebook-dir=/home/ubuntu/notebooks --ip='*' --port=${PORT} --no-browser
