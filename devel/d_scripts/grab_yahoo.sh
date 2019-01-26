f69316a8373bdb8b82b82da3ab19ca7eb256c9e1
tknpoon <tknpoon@gmail.com>
2019-01-26 22:56:11 +0800

    20190126-2256

--- devel/d_scripts/grab_yahoo.sh
+++ devel/d_scripts/grab_yahoo.sh
@@ -7,7 +7,7 @@ BNAME=$(basename $0 .sh)
 docker run \
 --env-file $HOME/.self_env \
 -e STAGE=${TAG_NAME:0:1} \
--i --rm \
+--rm \
 --network ${TAG_NAME:0:2}tptcn_overlay \
 -v $(cd $DIRNAME/;pwd)/${BNAME}.py:/tmp/entrypoint.py \
 tknpoon/private:${TAG_NAME:0:1}_anaconda \
